9a7995bc871ac24773db0d5e833e3f1e30f3775f
Victor Onah <onahvictorc@gmail.com>
2023-05-03 15:00:59 +0100

    feat: added formatted json error response to our router to handle method not found and method not allowed

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,12 @@ import (
 func (app *application) routes() http.Handler {
 	//initialiaing a our httprouter
 	mux := httprouter.New()
+	mux.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundResponse(w, r)
+	})
+	mux.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.methodNotAllowedResponse(w, r)
+	})
 
 	mux.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	mux.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
